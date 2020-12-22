79ede8bf2bb441db177bc75f361228f16bd0d730
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2020-12-22 22:16:47 +0700

    Design ProductController

--- Modules/Product/Http/Controllers/ProductController.php
+++ Modules/Product/Http/Controllers/ProductController.php
@@ -5,16 +5,44 @@ namespace Modules\Product\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Modules\Product\Models\Product;
+use Modules\Product\Models\ProductImage;
+use Modules\Product\Models\Tag;
+use Modules\Product\Models\ProductTag;
+use Modules\Category\Models\Category;
+use Modules\Product\Components\Recursive;
+use Modules\Product\Traits\StorageImageTrait;
+use Modules\Product\Traits\DeleteModelTrait;
+use Modules\Product\Http\Requests\ProductAddRequest;
+use Modules\Contact\Entities\Contact;
+use Modules\Slider\Models\Slider; 
 
 class ProductController extends Controller
 {
+    use StorageImageTrait, DeleteModelTrait;
+    private $category;
+    private $product;
+    private $productImage;
+    private $tag;
+    private $productTag;
+    public function __construct(Category $category, Product $product, ProductImage $productImage, Tag $tag, ProductTag $productTag)
+    {
+        $this->category = $category;
+        $this->product = $product;
+        $this->productImage = $productImage;
+        $this->tag = $tag;
+        $this->productTag = $productTag;
+    }
     /**
      * Display a listing of the resource.
      * @return Renderable
      */
     public function index()
     {
-        return view('product::index');
+        $products = $this->product->latest()->paginate(5);
+        return view('product::index', compact('products'));
     }
 
     /**
@@ -23,7 +51,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        return view('product::create');
+        $htmlOption = $this->getCategory($parentId = '');
+        return view('product::add', compact('htmlOption'));
     }
 
     /**
@@ -31,9 +60,53 @@ class ProductController extends Controller
      * @param Request $request
      * @return Renderable
      */
-    public function store(Request $request)
+    public function store(ProductAddRequest $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $dataProductCreate = [
+                'name' => $request->name,
+                'price' => $request->price,
+                'content' => $request->contents,
+                'category_id' => $request->category_id
+            ];
+            $dataUploadFeatureImage = $this->storageTraitUpload($request, 'feature_image_path', 'product');
+
+            if(!empty($dataUploadFeatureImage)) {
+                $dataProductCreate['feature_image_name'] = $dataUploadFeatureImage['file_name'];
+                $dataProductCreate['feature_image_path'] = $dataUploadFeatureImage['file_path'];
+            }
+            $product = $this->product->create($dataProductCreate);
+
+            // Insert data to product_images table
+            if ($request->hasFile('image_path')) {
+                foreach ($request->image_path as $fileItem) {
+                    $dataProductImageDetail = $this->storageTraitUploadMutiple($fileItem, 'product');
+                    $product->images()->create([
+                        'image_path' => $dataProductImageDetail['file_path'],
+                        'image_name' => $dataProductImageDetail['file_name']
+                    ]);
+                }
+            }
+
+            // Insert data to product_tags table
+            if (!empty($request->tags)) {
+                foreach($request->tags as $tagItem) {
+                    // Insert to tags
+                    $tagInstance = $this->tag->firstOrCreate(['name' => $tagItem]);
+                    $tagIds[] = $tagInstance->id;
+                }
+            }
+
+            $product->tags()->attach($tagIds);
+
+            DB::commit();
+            return redirect()->route('product.index');
+        } catch(\Exception $exception) {
+            DB::rollBack();
+            Log::error('Message: ' . $exception->getMessage() . " Line: " . $exception->getLine());
+        }
+
     }
 
     /**
@@ -53,7 +126,9 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        return view('product::edit');
+        $product = $this->product->find($id);
+        $htmlOption = $this->getCategory($product->category_id);
+        return view('product::edit', compact('htmlOption', 'product'));
     }
 
     /**
@@ -64,7 +139,53 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $dataProductUpdate = [
+                'name' => $request->name,
+                'price' => $request->price,
+                'content' => $request->contents,
+                'category_id' => $request->category_id
+            ];
+            $dataUploadFeatureImage = $this->storageTraitUpload($request, 'feature_image_path', 'product');
+
+            if(!empty($dataUploadFeatureImage)) {
+                $dataProductCreate['feature_image_name'] = $dataUploadFeatureImage['file_name'];
+                $dataProductCreate['feature_image_path'] = $dataUploadFeatureImage['file_path'];
+            }
+            $this->product->find($id)->update($dataProductUpdate);
+            $product = $this->product->find($id);
+
+            // Insert data to product_images table
+            if ($request->hasFile('image_path')) {
+                $this->productImage->where('product_id', $id)->delete();
+                foreach ($request->image_path as $fileItem) {
+                    $dataProductImageDetail = $this->storageTraitUploadMutiple($fileItem, 'product');
+                    $product->images()->create([
+                        'image_path' => $dataProductImageDetail['file_path'],
+                        'image_name' => $dataProductImageDetail['file_name']
+                    ]);
+                }
+            }
+
+            // Insert data to product_tags table
+            $tagIds = [];
+            if (!empty($request->tags)) {
+                foreach($request->tags as $tagItem) {
+                    // Insert to tags
+                    $tagInstance = $this->tag->firstOrCreate(['name' => $tagItem]);
+                    $tagIds[] = $tagInstance->id;
+                }
+            }
+
+            $product->tags()->sync($tagIds);
+
+            DB::commit();
+            return redirect()->route('product.index');
+        } catch(\Exception $exception) {
+            DB::rollBack();
+            Log::error('Message: ' . $exception->getMessage() . " Line: " . $exception->getLine());
+        }
     }
 
     /**
@@ -72,8 +193,27 @@ class ProductController extends Controller
      * @param int $id
      * @return Renderable
      */
-    public function destroy($id)
+    public function delete($id)
     {
-        //
+        return $this->deleteModelTrait($id, $this->product);
+    }
+
+    public function getCategory($parentId)
+    {
+        $data = $this->category->all();
+        $recursive = new Recursive($data);
+        $htmlOption = $recursive->categoryRecursive($parentId);
+        return $htmlOption;
+    }
+
+    public function search(Request $request) {
+        $products = $this->product->getSearchResult($request['productSearch']);
+        $contacts = Contact::all()->where('status', true);
+
+        $categories = Category::where('parent_id', 0)->latest();
+
+        $sliders = Slider::latest()->get();
+
+        return view('customer.index', compact('contacts', 'categories', 'products', 'sliders'));
     }
 }
